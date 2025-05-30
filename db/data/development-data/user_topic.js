ef2a4dff2a1e9e444656f1328a451b78dcbed6ad
Demim0nde <tamnorth@proton.me>
2025-05-30 13:16:16 +0100

    add test & development data for user_topic table

---
+++ db/data/development-data/user_topic.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { username: "tickle122", topic: "football" },
+  { username: "tickle122", topic: "cooking" },
+  { username: "grumpy19", topic: "football" },
+  { username: "happyamy2016", topic: "football" },
+  { username: "happyamy2016", topic: "cooking" },
+  { username: "cooljmessy", topic: "football" },
+  { username: "cooljmessy", topic: "cooking" },
+  { username: "jessjelly", topic: "cooking" },
+];
---
+++ db/data/test-data/user_topic.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { username: "butter_bridge", topic: "mitch" },
+  { username: "butter_bridge", topic: "cats" },
+  { username: "butter_bridge", topic: "paper" },
+  { username: "icellusedkars", topic: "cats" },
+  { username: "icellusedkars", topic: "paper" },
+  { username: "lurker", topic: "mitch" },
+];
