cbba1bf8fc574babee41dc538a903f075cbc0272
Demim0nde <tamnorth@proton.me>
2025-06-04 17:11:55 +0100

    make tests for GET /api/articles endpoint and make GET /api/articles endpoint functionality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,7 +7,7 @@ const app = require("../app.js");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
-// beforeAll(() => seed(data));
+beforeEach(() => seed(data));
 afterAll(() => db.end());
 
 describe("GET /api", () => {
@@ -18,5 +18,75 @@ describe("GET /api", () => {
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
       });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an object with the key of topics, and a value of all topic objects with their slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics).not.toHaveLength(0);
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  describe("200: Responds with an object containing an array of all articles", () => {
+    test("Responds with status code 200 and a non-empty array on the key of articles", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body: { articles } }) => {
+          expect(articles).not.toHaveLength(0);
+        });
+    });
+    test("Each article object contains all the articles columns, minus the body and plus a comment count", () => {
+      return request(app)
+        .get("/api/articles")
+        .then(({ body: { articles } }) => {
+          articles.forEach((article) => {
+            const {
+              article_id,
+              author,
+              title,
+              topic,
+              created_at,
+              votes,
+              article_img_url,
+              comment_count,
+            } = article;
+            expect(typeof article_id).toBe("number");
+            expect(typeof author).toBe("string");
+            expect(typeof title).toBe("string");
+            expect(typeof topic).toBe("string");
+            expect(typeof created_at).toBe("string");
+            expect(typeof votes).toBe("number");
+            expect(typeof article_img_url).toBe("string");
+            expect(typeof comment_count).toBe("number");
+          });
+        });
+    });
+    test("Articles are ordered by date in descending order", () => {
+      return request(app)
+        .get("/api/articles")
+        .then(({ body: { articles } }) => {
+          // console.log(articles);
+          for (let i = 1; i < articles.length; i++) {
+            const currentArticleDate = Number(
+              articles[i].created_at.split(/[\-T:\.Z]/).join("")
+            );
+            const previousArticleDate = Number(
+              articles[i - 1].created_at.split(/[\-T:\.Z]/).join("")
+            );
+            expect(previousArticleDate >= currentArticleDate).toBe(true);
+          }
+        });
+    });
   });
 });
--- app.js
+++ app.js
@@ -14,4 +14,36 @@ app.get("/api", (req, res) => {
   });
 });
 
+app.get("/api/topics", (req, res) => {
+  return db
+    .query(`SELECT * FROM topics`)
+    .then(({ rows }) => {
+      res.status(200).send({ topics: rows });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/api/articles", (req, res) => {
+  return db
+    .query(
+      `SELECT * FROM articles; SELECT article_id, COUNT(comment_id) FROM comments GROUP BY (article_id)`
+    )
+    .then(([{ rows: articles }, { rows: commentCount }]) => {
+      const commentCountLookup = {};
+      for (let article of commentCount) {
+        commentCountLookup[article.article_id] = article.count;
+      }
+      articles.forEach((article) => {
+        delete article.body;
+        const articleId = article.article_id;
+        article.comment_count = +commentCountLookup[articleId] || 0;
+      });
+      articles.sort((a, b) => {
+        return Number(b.created_at) - Number(a.created_at);
+      });
+      res.status(200).send({ articles: articles });
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = app;
