683df2a3e704c60ceb5a4680c602ea802e9a0d89
Demim0nde <tamnorth@proton.me>
2025-06-05 17:38:05 +0100

    refactor GET /api/articles/:article_id/comments endpoint

--- controllers/getCommentsByArticle.js
+++ controllers/getCommentsByArticle.js
@@ -1,16 +1,10 @@
 const { selectCommentsByArticle } = require("../models");
-const format = require("pg-format");
 
 const getCommentsByArticle = async (req, res, next) => {
   try {
     const articleId = Number(req.params["article_id"]);
-    const query = format(
-      `SELECT article_id FROM articles WHERE article_id = %L; SELECT * FROM comments WHERE article_id = %L`,
-      articleId,
-      articleId
-    );
-    const comments = await selectCommentsByArticle(articleId, query, res, next);
-    if (comments.length === 0) {
+    const comments = await selectCommentsByArticle(articleId, res, next);
+    if (comments[0].comment_id === null) {
       res.status(204).send();
     }
     res.status(200).send({ comments: comments });
--- models/selectCommentsByArticle.js
+++ models/selectCommentsByArticle.js
@@ -1,25 +1,22 @@
 const db = require("../db/connection");
 
-const selectCommentsByArticle = (articleId, query, res, next) => {
+const selectCommentsByArticle = (articleId, res, next) => {
   return db
-    .query(query)
-    .then(
-      ([
-        {
-          rows: [articleExists],
-        },
-        { rows },
-      ]) => {
-        if (articleExists) {
-          return rows;
-        } else {
-          return Promise.reject({
-            status: 404,
-            message: `No article found for article_id: ${articleId}`,
-          });
-        }
-      }
+    .query(
+      `SELECT articles.article_id AS article_exists, comments.* FROM comments FULL OUTER JOIN articles ON articles.article_id = comments.article_id WHERE articles.article_id = $1`,
+      [articleId]
     )
+    .then(({ rows }) => {
+      if (rows.length) {
+        console.log(rows);
+        return rows;
+      } else {
+        return Promise.reject({
+          status: 404,
+          message: `No article found for article_id: ${articleId}`,
+        });
+      }
+    })
     .catch((err) => next(err));
 };
 
