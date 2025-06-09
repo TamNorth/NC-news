1c9551061293c0e5c71e4a54c368403ccbf11e0c
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-09 12:12:31 +0100

    finish setup for hosting app via Supabase - update conneciton.js to properly export Pool and include script in package.json to seed production database

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+// const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -15,8 +15,9 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  console.log(`Connected to ${process.env.DATABASE_URL}`);
 } else if (ENV === "development" || ENV === "test") {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-module.exports = db;
+module.exports = new Pool(config);
