87eff0ae1688801d229bf82baa86649a61da4031
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-09 11:03:38 +0100

    start setup for hosting app via Supabase - update connection.js to refer to DATABASE_URL and update listen.js

--- listen.js
+++ listen.js
@@ -1,7 +1,9 @@
 const app = require("./app.js");
 
-app.listen("9090", (err) => {
-  if (!!err) {
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) {
     console.log(err);
   } else {
     console.log("listening on port 9090");
