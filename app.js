f567750a92d1e7b328b91f7a46adb5df99219801
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-23 10:52:43 +0100

    allow cross-origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const {
   getEndpoints,
   getTopics,
@@ -17,7 +18,13 @@ const {
   handleOtherErrors,
 } = require("./error-handlers");
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/api", (req, res) => getEndpoints(req, res));
 
